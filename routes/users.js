fd8343b5be1629d34fc96a2e39f84096788f74d3
Viraj Patidar <vrj381@gmail.com>
2020-09-24 23:34:29 +0530

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -11,31 +12,30 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next) => {
-  User.findOne({username: req.body.username})
-  .then((user) => {
-    if(user != null) {
-      var err = new Error('User ' + req.body.username + ' already exists!');
-      err.status = 403;
-      next(err);
-    }
-    else {
-      return User.create({
-        username: req.body.username,
-        password: req.body.password});
-    }
-  })
-  .then((user) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({status: 'Registration Successful!', user: user});
-  }, (err) => next(err))
-  .catch((err) => next(err));
-});
+// router.post('/signup', (req, res, next) => {
+//   User.findOne({username: req.body.username})
+//   .then((user) => {
+//     if(user != null) {
+//       var err = new Error('User ' + req.body.username + ' already exists!');
+//       err.status = 403;
+//       next(err);
+//     }
+//     else {
+//       return User.create({
+//         username: req.body.username,
+//         password: req.body.password});
+//     }
+//   })
+//   .then((user) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.json({status: 'Registration Successful!', user: user});
+//   }, (err) => next(err))
+//   .catch((err) => next(err));
+// });
 
 router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}), 
-    req.body.password, (err, user) => {
+  User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
     if(err) {
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
@@ -52,9 +52,10 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res) => {
