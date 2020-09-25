17cdbbd7f87bbabe3c3bedf4f23f70cd5c24c195
Viraj Patidar <vrj381@gmail.com>
2020-09-25 18:37:42 +0530

    Admin feature & Authentication

--- authenticate.js
+++ authenticate.js
@@ -1,22 +1,22 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('./models/user');
-var JwtStrategy = require('passport-jwt').Strategy;
-var ExtractJwt = require('passport-jwt').ExtractJwt;
-var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/user')
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const jwt = require('jsonwebtoken') // used to create, sign, and verify tokens
 
-var config = require('./config.js');
+const config = require('./config.js')
 
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
+exports.getToken = user => {
     return jwt.sign(user, config.secretKey, {expiresIn: 3600});
-};
+}
 
-var opts = {};
+const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
@@ -26,14 +26,23 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         User.findOne({_id: jwt_payload._id}, (err, user) => {
             if (err) {
                 return done(err, false);
-            }
-            else if (user) {
+            } else if (user) {
                 return done(null, user);
-            }
-            else {
+            } else {
                 return done(null, false);
             }
         });
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin)
+        next();
+
+    else {
+        var err = new Error('Only admin can do it');
+        err.status = 403;
+        return next(err);
+    }
+};
--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Leaderships = require('../models/leaders');
-var authenticate = require('../authenticate');
+const authenticate = require('../authenticate')
+const leaders = require('../models/leaders');
 
 
 const leaderRouter = express.Router();
@@ -10,7 +10,7 @@ leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
     .get((req, res, next) => {
-        Leaderships.find({})
+        leaders.find({})
             .then((leaders) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -18,8 +18,8 @@ leaderRouter.route('/')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .post(authenticate.verifyUser, (req, res, next) => {
-        Leaderships.create(req.body)
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        leaders.create(req.body)
             .then((leader) => {
                 console.log('leader Created ', leader);
                 res.statusCode = 200;
@@ -28,12 +28,12 @@ leaderRouter.route('/')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .put(authenticate.verifyUser, (req, res, next) => {
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /leaders');
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
-        Leaderships.remove({})
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        leaders.remove({})
             .then((resp) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -44,7 +44,7 @@ leaderRouter.route('/')
 
 leaderRouter.route('/:leaderId')
     .get((req, res, next) => {
-        Leaderships.findById(req.params.leaderId)
+        leaders.findById(req.params.leaderId)
             .then((dish) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -52,12 +52,12 @@ leaderRouter.route('/:leaderId')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .post(authenticate.verifyUser, (req, res, next) => {
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         res.statusCode = 403;
         res.end('POST operation not supported on /leaders/' + req.params.leaderId);
     })
-    .put(authenticate.verifyUser, (req, res, next) => {
-        Leaderships.findByIdAndUpdate(req.params.leaderId, {
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        leaders.findByIdAndUpdate(req.params.leaderId, {
             $set: req.body
         }, {new: true})
             .then((leader) => {
@@ -67,8 +67,8 @@ leaderRouter.route('/:leaderId')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
-        Leaderships.findByIdAndRemove(req.params.leaderId)
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        leaders.findByIdAndRemove(req.params.leaderId)
             .then((resp) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
